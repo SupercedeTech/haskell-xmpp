00de6654af89683ced8b7eb12fba6aa2e47b9907
Jappie Klooster <jappieklooster@hotmail.com>
2020-10-15 15:59:27 +0200

    Use similar config as riskbook with passwords changed

--- nix/ci-server.nix
+++ nix/ci-server.nix
@@ -1,11 +1,21 @@
 { pkgs
 , ... }:
-
 let
-  riskbook = import ../backend/riskbook-web {inherit settings; };
-  settings = pkgs.lib.recursiveUpdate (import ./settings.nix) {inherit pkgs;};
+
+  ejabberdInit = pkgs.runCommand "pg.new.sql-src" {} ''
+    tar xf "${pkgs.ejabberd.src}" ejabberd-*/sql/pg.new.sql
+    cp ejabberd-*/sql/pg.new.sql $out
+  '';
+  initSql = pkgs.writeText "init.sql" ''
+    CREATE USER "test-ejabberd" WITH SUPERUSER PASSWORD 'safe';
+    CREATE DATABASE ejabberd OWNER "test-ejabberd";
+
+    \c ejabberd riskbook-ejabberd
+    \i ${ejabberdInit}
+  '';
 in
-  {
+
+{
     virtualisation = {
       diskSize = 2048;
       writableStoreUseTmpfs = true;
@@ -25,7 +35,28 @@ in
         ejabberd = {
             enable = true;
             configFile = "/etc/ejabberd.yml";
+            package = pkgs.ejabberd.override {
+                withPgsql = true;
+            };
+        };
+        # we use postgres to get DIGEST-MD5 auth method.
+        # It'd be better if we could drop postgres for CI tests.
+        # (less complexity).
+        postgresql = {
+            enable = true;
+            port = 5432;
+            enableTCPIP = true;
+            initialScript = initSql;
+            package = pkgs.postgresql_9_6;
+            authentication = pkgs.lib.mkOverride 10 ''
+            host all all 127.0.0.1/8 trust
+            local all all trust
+            host ejabberd test-ejabberd ::1/128 trust
+            '';
         };
     };
-
-  }
+    systemd.services.ejabberd.after = [ "postgresql.service" ];
+    systemd.services.postgresql = {
+        requiredBy = [ "ejabberd.service" ];
+    };
+}
