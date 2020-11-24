e60c96346eb7d07663ed78a727662d462f3b39b1
Jappie Klooster <jappieklooster@hotmail.com>
2020-11-24 16:21:59 +0100

    Add spec for joining rooms

--- test/RoomSpec.hs
+++ test/RoomSpec.hs
@@ -1,9 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+
 -- | basic stuff like connecting to a room and sending messages
 module RoomSpec where
 
+import Network.XMPP.XEP.MUC
+import Data.Text(Text)
 import Control.Exception
 import Test.Hspec
 import Network.XMPP.Types
@@ -13,16 +19,34 @@ import Control.Monad.IO.Class
 import Control.Monad
 import Network.XMPP.Stream
 import Network.XMPP.Ejabberd
+import Network.XMPP.Types
 
 deriving instance Exception XmppError
 
 spec :: Spec
-spec = do
-  describe "connect to server" $ it "gets a connection" $ do
+spec = describe "ejabbered server tests" $ do
+  it "gets a connection" $ do
     handle <- liftIO $ connectViaTcp "localhost" 5222
     registerNewUser localEjabberdHost (EUser "jappie" "pass") (VHost "localhost")
     (result, stream) <- runXmppMonad $ initStream handle "localhost" "jappie" "pass" "someResource"
     nodeResource <- either throwIO pure result
     void $ runXmppMonad' stream closeStream
-  describe "connect to room" $ it "works" $ 1 `shouldBe` 1
-  describe "exchange messages" $ it "works" $ 1 `shouldBe` 1
+  it "can connect to a room" $ do
+    handle <- liftIO $ connectViaTcp "localhost" 5222
+    registerNewUser localEjabberdHost (EUser "jappie" "pass")  (VHost "localhost")
+    registerNewUser localEjabberdHost (EUser "jesiska" "pass") (VHost "localhost")
+    (result, stream) <- runXmppMonad $ do
+      jappie  <- either (error "init failure") id <$> initStream handle "localhost" "jappie"  "pass" "someResource"
+      xmppSend =<< withUUID (createRoomStanza jappie (mkParticipantJIDForRoom "jappie" $  mkRoomJID "localhost" "blah"))
+      xmppSend =<< withUUID (createRoomStanza jappie (mkParticipantJIDForRoom "jesiska" $  mkRoomJID "localhost" "blah"))
+    -- nodeResource <- either throwIO pure result
+    pure $! seq result -- no lazy
+    void $ runXmppMonad' stream closeStream
+  it "can exchange messages" $ 1 `shouldBe` 1
+
+mkRoomJID :: Server -> Text -> JID 'Node
+mkRoomJID srv roomId = NodeJID (NodeID roomId) $ DomainID $ "conference." <> srv
+
+mkParticipantJIDForRoom :: Username -> RoomJID -> RoomMemberJID
+mkParticipantJIDForRoom username NodeJID {..} =
+  NodeResourceJID nNode nDomain $ ResourceID username
